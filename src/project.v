03e02cdbcf8bc0fcc3584a23aae9f316ea05c151
shantanuw9 <149025403+shantanuw9@users.noreply.github.com>
2026-07-22 00:38:31 -0400

    Refactor output assignments to use zero

--- src/project.v
+++ src/project.v
@@ -18,8 +18,8 @@ module tt_um_shantanuw9 (
 
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 8'b0;
-  assign uio_oe  = 8'b0;
+  assign uio_out = 0;
+  assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
