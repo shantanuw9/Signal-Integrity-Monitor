12b7bf484f4e3d24d92817548aaaf7cd2aca314f
Shantanu Wad <shantanuwad@mac.mynetworksettings.com>
2026-07-23 01:26:23 -0400

    Wrote deadline_monitor verilog

--- src/deadline_monitor.v
+++ src/deadline_monitor.v
@@ -1,9 +1,28 @@
 `default_nettype
 
 module deadlineMonitor (
-
+    input wire clk,
+    input wire rst_n,
+    input wire sample_valid,
+    input wire [5:0] cfg_window,
+    output reg deadline_miss
 )
 
+reg [15:0] counter; // internal counter adjusted for 10 extra bits due to clock size
 
+always @(posedge clk) begin
+    deadline_miss <= 1'b0;
+    if(!rst_n) begin
+        counter <= 16'd0;
+    end else if(sample_valid) begin
+        counter <= 16'd0;
+    end else
+        counter <= counter + 1'b1;
+        if(counter > {cfg_window, 10'b0}) begin //base case of counter
+            deadline_miss <= 1'b1;
+        end
+    end
 
+end
 
+endmodule
