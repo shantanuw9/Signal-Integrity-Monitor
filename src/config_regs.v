80a2b7e192d5f4c657724e6bf9ae14fbb0a8b2c3
Shantanu Wad <shantanuwad@mac.mynetworksettings.com>
2026-07-23 02:12:16 -0400

    Wrote config_regs.v

---
+++ src/config_regs.v
@@ -0,0 +1,25 @@
+`default_nettype none
+
+module config_regs (
+    input wire clk,
+    input wire rst_n,
+    input wire [5:0] cfg_data, // ui_in[5:0]
+    input wire cfg_sel, // ui_in[6]
+    input wire cfg_strobe, // ui_in[7]
+    output reg [5:0] cfg_window,
+    output reg [5:0] cfg_jitter_thresh
+);
+
+always @(posedge clk) begin
+    if(!rst_n) begin
+        cfg_window <= 6'd20;
+        cfg_jitter_thresh <= 6'd5;
+    end else if (cfg_strobe) begin
+            if (!cfg_sel)
+                cfg_window        <= cfg_data;
+            else
+                cfg_jitter_thresh <= cfg_data;
+        end
+end
+
+endmodule
--- src/deadline_monitor.v
+++ src/deadline_monitor.v
@@ -1,12 +1,12 @@
-`default_nettype
+`default_nettype none
 
-module deadlineMonitor (
+module deadline_monitor (
     input wire clk,
     input wire rst_n,
     input wire sample_valid,
     input wire [5:0] cfg_window,
     output reg deadline_miss
-)
+);
 
 reg [15:0] counter; // internal counter adjusted for 10 extra bits due to clock size
 
@@ -16,7 +16,7 @@ always @(posedge clk) begin
         counter <= 16'd0;
     end else if(sample_valid) begin
         counter <= 16'd0;
-    end else
+    end else begin
         counter <= counter + 1'b1;
         if(counter > {cfg_window, 10'b0}) begin //base case of counter
             deadline_miss <= 1'b1;
